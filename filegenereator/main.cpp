c26982072059bac67f6bd91aaac1f3d3d35e1169
oosavu <egrbenev@gmail.com>
2020-06-01 19:28:48 +0300

    wip

--- filegenereator/main.cpp
+++ filegenereator/main.cpp
@@ -1,51 +1,161 @@
 #include <iostream>
-#include <iomanip>
 #include <string>
-#include <map>
 #include <random>
-#include <cmath>
 #include <array>
 #include <fstream>
+#include "cmdopts.h"
+#include <stdio.h>
+#include <chrono>
 
 using namespace std;
+
+struct Options
+{
+    int filesize{1000000000};
+    std::string filepath{"/home/oosavu/asd.txt"};
+    int stringRepeatSeries{1};
+    int stringRepeatSeriesLength{10};
+    int maxStrLen{100};
+    int maxNum{1000};
+};
 
 class GeneratorEngine
 {
 public:
-    GeneratorEngine(int maxNum = 1000, int minStrLen = 1, int maxStrLen = 10) :
-        m_maxStrLen(maxStrLen),
-        m_numDistributor(0, maxNum),
-        m_strLenDistributor(minStrLen, maxStrLen),
+    GeneratorEngine(const Options &opts) :
+        m_generator(time(0)),
+        m_numDistributor(0, opts.maxNum),
+        m_strLenDistributor(1, opts.maxStrLen),
         m_charDistributor(0, posibleChars.size() - 1)
     {
     }
 
-    void write(ofstream &file)
+    inline std::string genNum()
     {
-        int num = m_numDistributor(m_generator);
-        char str[m_maxStrLen + 1];
+        return std::to_string(m_numDistributor(m_generator)) + ". ";
+    }
+
+    inline std::string genString()
+    {
+        //return "sssssssssssssdfsdfffffffffffffffff";
+        std::string str;
         size_t len = m_strLenDistributor(m_generator);
+        str.resize(len + 1);
         for(size_t i = 0; i < len; i++)
             str[i] = posibleChars[m_charDistributor(m_generator)];
-        str[len] = 0x00;
-        file << std::to_string(num) << ". " << str << endl;
+        str[len] = '\n';
+        return str;
     }
+
+//    void writeRandom(ofstream &file)
+//    {
+//        int num = genNum();
+//        std::string str = genString();
+//        //file << ". " << str;
+//        file << std::to_string(num) << ". " << str;
+//    }
 private:
-    std::vector<char> stringBuf;
-    size_t m_maxStrLen;
-    std::array<char, 6> posibleChars = {'a', 'b', 'c', 'd', 'e', 'f'};
+    std::array<char, 27> posibleChars ={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '};
     std::mt19937 m_generator;
     std::uniform_int_distribution<> m_numDistributor;
     std::uniform_int_distribution<> m_strLenDistributor;
     std::uniform_int_distribution<> m_charDistributor;
 };
 
-int main()
+int main(int argc, const char* argv[])
 {
-    GeneratorEngine engine;
-    std::ofstream file("/home/oosavu/example.bin", ios::binary);
-    for(int i = 0; i < 5; i++)
-        engine.write(file);
-    file.close();
+    auto parser = CmdOpts<Options>::Create({{"--filesize", &Options::filesize },
+                                            {"--filepath", &Options::filepath },
+                                            {"--stringRepeatSeries", &Options::stringRepeatSeries},
+                                            {"--stringRepeatSeriesLength", &Options::stringRepeatSeriesLength},
+                                            {"--maxStrLen", &Options::maxStrLen},
+                                            {"--maxNum", &Options::maxNum}});
+
+    auto opts = parser->parse(argc, argv);
+
+    //GeneratorEngine engine(opts);
+    //std::ofstream file(opts.filepath, ios::binary);
+    //std::vector<char> vec(23);
+    //file.rdbuf()->pubsetbuf(&vec.front(), vec.size());
+
+    //    if(!file)
+    //    {
+    //        cerr << "ERROR: can't open file:" << opts.filepath;
+    //        return 0;
+    //    }
+
+    int qwe = 100;
+    {
+        FILE* pFile;
+        pFile = fopen("file.binary", "wb");
+        setvbuf ( pFile , NULL , _IOFBF , 1024 );
+        std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
+        string repeat;
+        int qwe = 10;
+        repeat.resize(qwe, 'a');
+        int coun = 0;
+        while(coun < opts.filesize)
+        {
+            coun += qwe;
+            fwrite(repeat.data(), sizeof(char), repeat.size() , pFile);
+        }
+        fflush(pFile);
+        fclose (pFile);
+        std::chrono::system_clock::time_point currentTime = std::chrono::system_clock::now();
+        int time = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime).count();
+        float bytesPerSecond = float(opts.filesize) / (float(time) / 1000.0);
+        cout << "FINISH C. time:" << time << " msec. speed: " << int(bytesPerSecond)  << " bytes/sec" << endl;
+    }
+
+    {
+        std::ofstream file(opts.filepath, ios::binary);
+        std::vector<char> vec(23);
+        std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
+        string repeat;
+
+        repeat.resize(qwe, 'a');
+        int coun = 0;
+        while(coun < opts.filesize)
+        {
+            coun += qwe;
+            file.write(repeat.data(), repeat.size());
+            //fwrite(repeat.data(), sizeof(char), repeat.size() , pFile);
+        }
+        file.close();
+        std::chrono::system_clock::time_point currentTime = std::chrono::system_clock::now();
+        int time = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime).count();
+        float bytesPerSecond = float(opts.filesize) / (float(time) / 1000.0);
+        cout << "FINISH. time:" << time << " msec. speed: " << int(bytesPerSecond)  << " bytes/sec" << endl;
+    }
+
+    //just put repeats at the begining of file (TODO ability to distribute it)
+    //    for(int i = 0; i < opts.stringRepeatSeries && file.tellp() < opts.filesize; i++)
+    //    {
+    //        string repeat = engine.genString();
+    //        for(int j = 0; j < opts.stringRepeatSeriesLength && file.tellp() < opts.filesize; j++)
+    //        {
+    //            int num = engine.genNum();
+    //            file << std::to_string(num) << ". " << repeat;
+    //        }
+    //    }
+
+    //string repeat = engine.genString();
+
+
+
+    //while(file.tellp() < opts.filesize)
+    //file << repeat;
+    //    while(coun < opts.filesize)
+    //    {
+    //        coun += qwe;
+    //        file.write(repeat.data(), repeat.size());
+    //    }
+    //        //engine.writeRandom(file);
+    //    file.close();
+
+
+
+
     return 0;
 }
+
--- sorter/main.cpp
+++ sorter/main.cpp
@@ -14,13 +14,13 @@
 // it's bad practice to use namespace std, however for test job it's ok
 using namespace std;
 
-std::vector<size_t> findChunkBounds(std::ifstream &file, size_t averageChunkSize)
+vector<size_t> findChunkBounds(ifstream &file, size_t averageChunkSize)
 {
-    file.seekg (0, file.end);
+    file.seekg(0, file.end);
     size_t fileSize = file.tellg();
     cout << "input file size:" << fileSize << endl;
 
-    std::vector<size_t> res;
+    vector<size_t> res;
     res.push_back(0);
 
     size_t averageGlobalPos = 0;
@@ -33,8 +33,8 @@ std::vector<size_t> findChunkBounds(std::ifstream &file, size_t averageChunkSize
             break;
         }
         file.seekg(averageGlobalPos, file.beg);
-        std::string line;
-        std::getline(file, line);
+        string line;
+        getline(file, line);
         res.push_back(file.tellg());
         if(file.eof())
             break;
@@ -51,14 +51,14 @@ struct LineInfo{
 
 };
 
-std::vector<size_t> sortIndexes(const std::vector<LineInfo> &lineData, const std::vector<char> &data)
+vector<size_t> sortIndexes(const vector<LineInfo> &lineData, const vector<char> &data)
 {
-    std::vector<size_t> idx(lineData.size());
+    vector<size_t> idx(lineData.size());
     iota(idx.begin(), idx.end(), 0);
     const char* rawData = data.data();
     sort(idx.begin(), idx.end(),
          [&](size_t l1, size_t l2) {
-        int cmp = std::strcmp(&rawData[lineData[l1].strStart], &rawData[lineData[l2].strStart]);
+        int cmp = strcmp(&rawData[lineData[l1].strStart], &rawData[lineData[l2].strStart]);
         if (cmp < 0)
             return true;
         else if (cmp > 0)
@@ -70,31 +70,22 @@ std::vector<size_t> sortIndexes(const std::vector<LineInfo> &lineData, const std
     return idx;
 }
 
-//cout << lineInfo.start <<endl;
-//size_t lineEnd = distance(data.begin(), it);
-// std::from_chars() does not supported in gcc75
-// std::string_view does not supported by stoi
-// stoi does not support char*
-//while(rawData[lineInfo.strStart] != '.')
-//    lineInfo.strStart++;
-
-
-std::vector<LineInfo> collectChunkInfo(std::vector<char> &data)
+vector<LineInfo> collectChunkInfo(vector<char> &data)
 {
-    std::vector<LineInfo> lines;
+    vector<LineInfo> lines;
     auto i = data.begin(), end = data.end();
     //end--; //drop the last '\n'
     char* rawData = data.data();
     while(i != end)
     {
         LineInfo lineInfo;
-        lineInfo.start = std::distance(data.begin(), i);
-        lineInfo.num =  static_cast<unsigned int>(std::atoi(&rawData[lineInfo.start]));
-        auto dotPosition = std::find(i, end, '.');
+        lineInfo.start = distance(data.begin(), i);
+        lineInfo.num =  static_cast<unsigned int>(atoi(&rawData[lineInfo.start]));
+        auto dotPosition = find(i, end, '.');
         dotPosition ++; // skip dot
         //dotPosition ++; // skip space
         lineInfo.strStart = distance(data.begin(), dotPosition);
-        i = std::find(dotPosition, end, '\n');
+        i = find(dotPosition, end, '\n');
         i++;
         lineInfo.finis = distance(data.begin(), i);
         cout << rawData[lineInfo.strStart] << endl;
@@ -103,9 +94,9 @@ std::vector<LineInfo> collectChunkInfo(std::vector<char> &data)
     return lines;
 }
 
-void saveSortedChunk(const std::vector<size_t> &idx, const std::vector<LineInfo> lineInfo, char* rawData, const std::string &filePath)
+void saveSortedChunk(const vector<size_t> &idx, const vector<LineInfo> lineInfo, char* rawData, const string &filePath)
 {
-    std::ofstream file(filePath, std::ios::binary);
+    ofstream file(filePath, ios::binary);
     for(size_t i : idx)
     {
         file.write(&rawData[lineInfo[i].start], lineInfo[i].finis - lineInfo[i].start);
@@ -113,28 +104,28 @@ void saveSortedChunk(const std::vector<size_t> &idx, const std::vector<LineInfo>
     file.close();
 }
 
-void sortChunks(std::ifstream &file, std::vector<size_t> chunkBounds)
+void sortChunks(ifstream &file, vector<size_t> chunkBounds)
 {
     file.seekg (0, file.beg);
     for(size_t chunkIndex = 0; chunkIndex < chunkBounds.size() - 1; chunkIndex++)
     {
         size_t chunkSize = chunkBounds[chunkIndex + 1] - chunkBounds[chunkIndex];
-        std::vector<char> data(chunkSize);
+        vector<char> data(chunkSize);
         file.read(data.data(), data.size());
-        std::vector<LineInfo> lineData = collectChunkInfo(data);
-        std::vector<size_t> idx = sortIndexes(lineData, data);
+        vector<LineInfo> lineData = collectChunkInfo(data);
+        vector<size_t> idx = sortIndexes(lineData, data);
         for(size_t i : idx)
             cout << i  <<  ": "<< lineData[i].num << data[lineData[i].strStart]<< endl;
         saveSortedChunk(idx, lineData, data.data(), "/home/oosavu/qwe.txt");
     }
 }
 
-void createSortedChunks(const std::string &filePath)
+void createSortedChunks(const string &filePath)
 {
-    std::ifstream file(filePath, std::ios::binary);
+    ifstream file(filePath, ios::binary);
     if(!file)
-        throw std::string("can't open file:") + filePath.c_str();
-    std::vector<size_t> chunkBounds = findChunkBounds(file, 1000000);
+        throw string("can't open file:") + filePath.c_str();
+    vector<size_t> chunkBounds = findChunkBounds(file, 1000000);
     sortChunks(file, chunkBounds);
 }
 
@@ -143,8 +134,8 @@ int main()
 {
     try {
         createSortedChunks("/home/oosavu/example.bin");
-    } catch (std::string s) {
-        std::cerr << "ERROR:" << s << endl;;
+    } catch (string s) {
+        cerr << "ERROR:" << s << endl;;
         return -1;
     }
     return 0;
